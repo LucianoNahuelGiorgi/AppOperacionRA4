a5b8f4a9afb501a02fae3f2cdee8c77cde88c55f
LucianoNahuelGiorgi <lucianogiorgi@outlook.com.ar>
2022-01-10 20:41:47 -0300

    Validacion numeros enteros y numeros reales con JavaScript

--- src/main/resources/static/js/validaciones.js
+++ src/main/resources/static/js/validaciones.js
@@ -1,5 +1,54 @@
-valor = document.getElementById("indicacionFATCA").value;
+function validacionEnvio() {
+	valor = document.getElementById("indicacionFATCA").value;
+	if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
+		alert('[ERROR] El campo debe tener un valor de...');
 
-if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-  return false;
+		return false;
+	}
+
+	return true;
 }
+
+
+function validacionNumEntero(evt) {
+	var charCode = (evt.which) ? evt.which : evt.keyCode
+
+	if (charCode > 47 && charCode < 58)
+		return true;
+	else
+		console.log("Por favor, ingresar sólo números enteros");
+
+	return false;
+}
+
+function validacionNumReal(evt) {
+	let charCode = (evt.which) ? evt.which : evt.keyCode;
+	let campo = document.getElementById("temperaturaN");
+	let cadena = campo.value;
+	console.log(cadena);
+
+	if (charCode == 46 && campo.value == "") {
+
+		return false;
+	} else {
+		if ((charCode > 47 && charCode < 58) || charCode == 46) {
+			if ((charCode == 46) && (cadena.includes(".", 0))) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+		else {
+			console.log("Por favor, ingresar sólo números y/o un solo punto decimal");
+			return false;
+		}
+	}
+}
+
+/*
+function validacionNotCientifica(evt) {
+	let charCode = (evt.which) ? evt.which : evt.keyCode;
+
+}
+*/
